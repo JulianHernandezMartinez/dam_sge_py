8a6626246b8efce209927fb88c2693d4fb811d2e
JulianHernandezMartinez <profefp2223@gmail.com>
2023-01-17 10:14:00 +0100

    Renombrado de carpetas y cambio de prim. practic

--- 2_practicas/1_enunciados/0_0_intercambio_f_var.py
+++ 2_practicas/1_enunciados/0_0_intercambio_f_var.py
@@ -1,6 +1,16 @@
+# Este primer ejercicio es de ejemplo
+# Se incluye la parte comentada con "#"
+# Y la solución
+# 
+# Enunciado
+#
 # haz un programa que pida dos numeros X e Y y que los intercambie
 # y presente una salida del tipo "número 1 es Y , número 2 es X"
-
+#
+# 
+# Escribe tu solucion aqui: 
+#
+# SOLUCION
 
 num1 = int(input("número 1? "))
 num2 = int(input("número 2? "))
