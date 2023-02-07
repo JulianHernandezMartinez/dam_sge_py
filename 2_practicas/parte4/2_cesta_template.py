64b9cf8c2b61616e0db32efa7a921541e7c4bd57
JulianHernandezMartinez <profefp2223@gmail.com>
2023-02-07 15:39:35 +0100

    nueva practica

---
+++ 2_practicas/parte4/2_cesta_template.py
@@ -0,0 +1,70 @@
+# GLOBALES
+# Añade las variables globales que necesites para tu programa
+
+
+# FUNCIONES
+# Añade las funciones que llamarás en la zona MAIN
+
+def verCesta():
+    # mostrar la lista de elementos con nombres y cantidades
+    # si no hay ítems, mostrar un mensaje diga que no hay
+    # no devuelve nada
+    pass
+
+
+def añadirItem(nom, num):
+    # añadir una cantidad a un ítem, modificarlo si ya existe
+    # nombre es el nombre del elemento a añadir, num la cantidad
+    # no devuelve nada
+    pass
+
+
+def quitarItem(nom, num):
+    # nombre es el nombre del elemento a quitar, num la cantidad
+    # devolver True si se ha podido quitar, False de lo contrario
+    pass
+
+
+def buscarItems(nom):
+    # buscar un ítem por nombre
+    # devolver la cantidad actual, cero si no hay
+    pass
+
+
+def quitarItems():
+    # vaciar la lista de elementos
+    # no devuelve nada
+    pass
+
+
+def mostrarMenu():
+    # mostrar la lista de opciones
+    # no devuelve nada
+    pass
+
+
+def inputText(text):
+    # pide un string no vacío
+    # texto es el mensaje que invita a hacer la entrada
+    # devuelve el texto introducido
+    pass
+
+
+def inputNumero(text, min, max):
+    # pide un entero entre min y max
+    # texto es el mensaje que invita a hacer la entrada
+    # si el número no está entre min y max, vuelve a preguntar
+    # devuelve el número introducido
+    pass    
+
+# MAIN
+# Bucle principal del programa que va llamando las funciones
+
+# mostrar el menú, de 1 a 6:
+# 1: Listar items
+# 2: Añadir item
+# 3: Quitar item
+# 4: Buscar item
+# 5: Vaciar lista
+# 6: Salir
+# y ejecutar la tarea indicada
